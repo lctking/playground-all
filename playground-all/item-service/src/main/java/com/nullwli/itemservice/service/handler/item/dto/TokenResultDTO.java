bc0bddd0c2d9d9820b5feecb76a7a33cf9e5fc26
LLWW04 <1713673102@qq.com>
2024-11-16 16:04:58 +0800

    更新：若令牌数不满足需求，则记录对应商品id和令牌数

--- playground-all/item-service/src/main/java/com/nullwli/itemservice/service/handler/item/dto/TokenResultDTO.java
+++ playground-all/item-service/src/main/java/com/nullwli/itemservice/service/handler/item/dto/TokenResultDTO.java
@@ -2,10 +2,16 @@ package com.nullwli.itemservice.service.handler.item.dto;
 
 import lombok.*;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenResultDTO {
     private Boolean isSuccess;
+
+    private Boolean tokenHasNone;
+
+    private List<String> NoneTokenInfo;
 }
